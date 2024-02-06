cf83ff24e3b727229cbcece3b2d86d08401c2aa0
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-06 16:39:11 +0100

    Fine Pari o dispari!

---
+++ pari e dispari/js/script.js
@@ -0,0 +1,61 @@
+let userChoice;
+let userNumber;
+let computerChoice;
+let sum;
+let result;
+let winner;
+
+
+function randomComputer5() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+
+
+
+function pariODispari() {
+    if (sum % 2 == 0) {
+        return "Pari";
+    } else {
+        return "Dispari";
+    }
+}
+
+
+
+
+
+let myButton = document.querySelector("button").addEventListener("click",
+    function () {
+
+        computerChoice = randomComputer5();
+        console.log(computerChoice)
+        userNumber = Number(document.getElementById("userNumber").value);
+        sum = computerChoice + userNumber;
+        console.log("La somma è " + sum)
+        result = pariODispari();
+        console.log("il risultato é " + result);
+
+        userChoice = document.getElementById("userChoice").value;
+
+        if (userChoice.toUpperCase() !== result.toUpperCase()) {
+            winner = document.querySelector("p").innerHTML = `${result}! <br> Hai perso perché hai scelto ${userChoice}! <br> Il computer ha scelto ${computerChoice} e tu hai scelto ${userNumber}! <br> Ritenta!`
+        } else {
+            winner = document.querySelector("p").innerHTML = `${result}! <br> Hai vinto perché hai scelto ${userChoice}! <br> Il computer ha scelto ${computerChoice} e tu hai scelto ${userNumber}! <br> Complimenti!`
+
+        }
+
+        document.getElementById("userNumber").value = "";
+        document.getElementById("userChoice").value = "";
+
+    }
+)
+
+
+
+
+
+
+
+
+
